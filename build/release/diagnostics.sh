3718580246e5d390335e7170bbc48db4bcf86abb
Paul Bastide <pbastide@us.ibm.com>
2020-01-19 10:00:34 -0500

    here
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>

--- build/release/diagnostics.sh
+++ build/release/diagnostics.sh
@@ -32,6 +32,7 @@ popd > /dev/null
 diagnostic_details > diagnostic_details.log
 
 # To include the distribution, include ' -or -iname fhir-server-distribution.zip'
-tar -czf release-test-coverage-and-logs.tgz $(find . -iname diagnostic_details.log -or -iname surefire-reports -or -iname jacoco.exec -or -iname jacoco-aggregate)
+# or '-or -iname surefire-reports -or -iname jacoco.exec'
+tar -czf release-test-coverage-and-logs.tgz $(find . -iname diagnostic_details.log -or -iname jacoco-aggregate)
 
 # EOF
